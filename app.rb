6fd21bddc792b79322c740d0b78da2aa562ddade
Andrew Haust <andrewwhhaust@gmail.com>
2016-06-17 12:55:18 -0400

    Add Product, LineItem, and TaxCalculator

--- app.rb
+++ app.rb
@@ -8,3 +8,32 @@ module Database
     Product.new(data)
   end
 end
+
+# OpenStruct allows accessing hash keys as methods
+class Product < OpenStruct
+end
+
+class LineItem
+  attr_reader :product, :description, :type, :price, :quantity
+
+  def initialize(product, quantity = 1)
+    @product = product
+    @description = product.description
+    @price = product.price
+    @type = product.type
+    @quantity = quantity
+  end
+
+  def imported?
+    @product.imported
+  end
+end
+
+module TaxCalculator
+  extend self
+
+  def taxable?(item)
+    return true if item.imported?
+    !UNTAXABLE_TYPES.include?(item.type)
+  end
+end
--- app_test.rb
+++ app_test.rb
@@ -9,3 +9,27 @@ describe Database do
     product.price.must_equal 12.49
   end
 end
+
+describe LineItem do
+  it "takes a product and knows its tax" do
+    line_item = LineItem.new Database.find_product(6)
+    line_item.tax.must_equal 2.8
+  end
+end
+
+describe TaxCalculator do
+  it "knows that an item is taxable" do
+    line_item = LineItem.new Database.find_product(5)
+    TaxCalculator.taxable?(line_item).must_equal true
+  end
+
+  it "knows that an item is untaxable" do
+    line_item = LineItem.new Database.find_product(8)
+    TaxCalculator.taxable?(line_item).must_equal false
+  end
+
+  it "knows that an imported item is always taxable" do
+    line_item = LineItem.new Database.find_product(6)
+    TaxCalculator.taxable?(line_item).must_equal true
+  end
+end
